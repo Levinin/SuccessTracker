30dd17bdb0204f198f47b8d1dd59ba2d7ad08b34
Andy Edmondson <andrew.edmondson@gmail.com>
2023-09-04 16:14:40 +0100

    Rebasing with database

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -13,30 +13,49 @@ class StreamlitUI:
     def __init__(self, task_tracker):
         self.task_tracker = task_tracker
         self.current_user = ""
+        self.current_record = None
 
     def run(self):
         st.set_page_config(page_title="Success Tracker", page_icon="ico.png")
 
+        if 'username' not in st.session_state:
+            st.session_state.username = None
+
+        if 'password' not in st.session_state:
+            st.session_state.password = None
+
+        if 'logged_in' not in st.session_state:
+            st.session_state.logged_in = False
+
+        if 'date' not in st.session_state:
+            st.session_state.date = None
+
+        if 'slips' not in st.session_state:
+            st.session_state.slips = []
+
         st.sidebar.title(f'Welcome to Success Tracker!')
 
         # Login
-        username = st.sidebar.text_input("Username")
-        password = st.sidebar.text_input("Password", type='password')
-        if st.sidebar.button("Login"):
-            if username == "user" and password == '1234':
-                st.sidebar.success("Logged In as {}".format(username))
-                task_tracker = TaskTracker()
-                self.current_user = username
+        st.session_state.username = st.sidebar.text_input("Username")
+        st.session_state.password = st.sidebar.text_input("Password", type='password')
+        self.task_tracker.hash_uname_password(st.session_state.username, st.session_state.password)
+        if st.session_state.logged_in:
+            self.current_user = st.session_state.username
+            self.main()
+        else:
+            if st.sidebar.button("Login") and not st.session_state.logged_in:
+                if self.task_tracker.get_record(st.session_state.username, st.session_state.password):
+                    st.sidebar.success("Logged in as {}".format(st.session_state.username))
+                    self.current_user = st.session_state.username
+                else:
+                    st.sidebar.success("Welcome new user: {}".format(st.session_state.username))
+                    self.current_user = st.session_state.username
+                st.session_state.logged_in = True
                 self.main()
-            else:
-                st.sidebar.warning("Incorrect Username/Password")
 
     def main(self):
 
         st.title(f":crown: {self.current_user}'s Success Journey")
-
-        # Load state from file
-        self.task_tracker.load_from_file()
 
         # Set start date in the task tracker
         self.task_tracker.set_start_date(st.date_input('Success Start Date', value=self.task_tracker.start_date))
@@ -48,18 +67,24 @@ class StreamlitUI:
         # Failed Button
         if fail_button:
             if self.task_tracker.start_date is not None:
-                today = date.today()
-                self.task_tracker.increment_failed_day(today)
+                # today = date.today()
+                # self.task_tracker.increment_failed_day(today)
+                slips = st.session_state.slips
+                slips.append(date.today())
+                st.session_state.slips = slips
 
         # Adjust Button
         if adjust_button:
             if self.task_tracker.start_date is not None:
-                self.task_tracker.adjust_failed_day()
-
-        st.write(f'You have slipped up {self.task_tracker.get_num_failed_days()} times.')
+                # self.task_tracker.adjust_failed_day()
+                slips = st.session_state.slips
+                slips.pop()
+                st.session_state.slips = slips
 
         # Calculate and display success percentage
-        success_percentage = self.task_tracker.calculate_success_percentage()
+        success_percentage = self.task_tracker.calculate_success_percentage(st.session_state.slips)
+
+        st.write(f'You have slipped up {len(self.task_tracker.failed_days)} times.')
 
         encourage = f"### {random.choice(motivational_statements)}"
         exercise = f"### :green[Today, if you feel an urge:]\n#### :green[{random.choice(all_exercises)}]"
@@ -69,12 +94,11 @@ class StreamlitUI:
         st.progress(int(success_percentage), "Your personal victory bar")
         st.markdown(exercise)
 
-        # Save state to file
-        self.task_tracker.save_to_file()
+        # Save state to db
+        self.task_tracker.save_to_db(st.session_state.slips)
 
 
 if __name__ == '__main__':
-    tracker = TaskTracker()
-    ui = StreamlitUI(tracker)
+    ui = StreamlitUI(TaskTracker())
     ui.run()
 
--- task_tracker.py
+++ task_tracker.py
@@ -1,6 +1,14 @@
+# The task tracker object.
+# Keeps data in an sqlite3 database.
+# Schema is:
+# CREATE TABLE user_data (
+# id BLOB PRIMARY KEY,
+
 from datetime import date, timedelta
 import json
 import sqlite3
+import hashlib
+import pickle
 
 
 class TaskTracker:
@@ -8,7 +16,24 @@ class TaskTracker:
         self.start_date = None
         self.failed_days = []
         self.save_file_name = 'tracker_data.json'
+        self.db_name = "user_data.db"
+        self.hash = ""
+        self.db_conn = self.connect_to_db()
+        self.db_curr = self.db_conn.cursor()
+        self.current_record = None
 
+    def connect_to_db(self):
+        return sqlite3.connect(self.db_name)
+
+    def get_record(self, uname, password) -> bool:
+        print(self.hash)
+        self.db_curr.execute("SELECT * FROM user_data WHERE id = ? LIMIT 1;", (self.hash,))
+        self.current_record = self.db_curr.fetchone()
+        if self.current_record:
+            self.start_date = date.fromisoformat(self.current_record[1])
+            self.failed_days = pickle.loads(self.current_record[2])
+            return True
+        return False
 
     def set_start_date(self, start_date):
         self.start_date = start_date
@@ -19,7 +44,14 @@ class TaskTracker:
     def adjust_failed_day(self):
         self.failed_days = self.failed_days[:-1]
 
-    def calculate_success_percentage(self):
+    def hash_uname_password(self, uname, password):
+        hash_function = hashlib.sha3_512()
+        hash_function.update(uname.encode('utf-8'))
+        hash_function.update(password.encode('utf-8'))
+        self.hash = hash_function.hexdigest()
+
+    def calculate_success_percentage(self, slip_list):
+        self.failed_days = slip_list
         today = date.today()
         # Success days are all days between start date and today or 100 days, whichever is smaller
         # Calculate the number of days between start date and today
@@ -39,6 +71,17 @@ class TaskTracker:
 
         # Calculate the percentage
         return (success_days / total_days) * 100
+
+    def save_to_db(self, _slips):
+        """Write the hash, start date and failed days to the database. Catch a write failure and return False."""
+        start = str(self.start_date)
+        slips = pickle.dumps(_slips)
+        # print(start, self.failed_days)
+
+        self.db_curr.execute(f"INSERT OR IGNORE INTO user_data(id, start, slips) VALUES (?,?,?);",
+                             (self.hash, start, slips))
+        self.db_curr.execute(f"UPDATE user_data SET start = {start}, slips = {slips} WHERE id = {self.hash};")
+        self.db_conn.commit()
 
     def save_to_file(self):
         data = {
@@ -62,3 +105,5 @@ class TaskTracker:
             self.start_date = date.today()
             pass
 
+    def __del__(self):
+        self.db_conn.close()
